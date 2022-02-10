93ddd7aadfe8383500be9f7fc915e96bfcb50881
Sharon <fubzee@gmail.com>
2022-02-11 00:08:58 +1100

    Fixed bugs with ReadMe

---
+++ README.md
@@ -0,0 +1,37 @@
+# README generator
+
+   
+
+
+---
+Generates a README file
+---
+## Table of Contents: 
+
+* [Installation](#Installation)
+* [Licence](#License)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Testing](#Testing)
+* [GIT Profile](# GIT Profile)
+* [Questions](#questions)
+---
+### Installation :exclamation:
+In order to install please use:
+ Install NPM and run in node
+---
+### Contributing
+ No
+---
+### Licence
+This project is licensed under:
+  
+---
+### Testing
+None
+---
+### GIT Profile :link: 
+This project can be found at : https://github.com/fubzee/W9HW-ReadMe
+---
+### Questions :question:
+* If you have any questions about this application please email me at smarti-dev@gmail.com
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,28 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+const { error } = require("console");
+const fs = require(`fs`);
 
-function renderLicenseBadge(license) {
+function renderLicenseBadge(badge) {
 
-  switch(license){
+  switch(badge){
 
-    case 'NPM(inquirer)':
-       var badge = `https://img.shields.io/npm/l/inquirer`;
+    case 'NPM':
+       var badge = `(https://img.shields.io/badge/license-npm-red.svg)`;
        break;
-    case 'NPM(fs)':
-      var badge =  'https://img.shields.io/npm/l/fs';
+    case 'GNU':
+      var badge =  `(https://img.shields.io/badge/License-GNUv3-blue.svg)`;
+      break;
+    case 'MIT':
+      var badge =  `(https://img.shields.io/badge/License-MIT-yellow.svg)`;
+      break;
+    case 'MPL':
+      var badge =  `(https://img.shields.io/badge/License-MPL-yellow.svg)`;
       break;
     default:
       return badge = ` `
   }
+  return badge;
 }  
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -21,58 +30,77 @@ function renderLicenseLink(license) {
 
   switch(license){
 
-    case 'NPM(inquirer)':
-       var liclink = `https://img.shields.io/npm/l/inquirer`;
+    case `NPM`:
+       var liclink = `![License npm]https://img.shields.io/npm/1/inquirer`;
        break;
-    case 'NPM(fs)':
-      var liclink =  'https://img.shields.io/npm/l/fs';
+    case `GNU`:
+      var liclink =  `![License: GPL v3]https://img.shields.io/gnu`;
+      break;
+    case `MPL`:
+      var liclink = `![License: MIT]https://img.shields.io/MPL/l/fs`; 
+      break;
+    case `MIT`:
+      var liclink = `![License: MIT]https://img.shields.io/MIT/l/fs`;
       break;
     default:
       return liclink= ` `
   }
+  return liclink;
 } 
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   
-       renderLicenseBadge(license);
-       renderLicenseLink(license);
+ //      renderLicenseBadge(license);
+ //      renderLicenseLink(license);
 
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown (answers) {
-  renderLicenseSection(data.license);
+//  renderLicenseSection(answers.license);
   var readMe = 
-          `# ${data.title}
-            ${badge}
-            ${data.description}
-            ---
-          ## Table of Contents:  
-          * [Installation](#Installation)
-          * [Licence](#License)
-          * [Usage](#usage)
-          * [Contributing](#contributing)
-          * [Testing](#Testing)
-          * [GIT Profile](# GIT Profile)
-          * [Questions](#questions)
-          ### Installation:
-          In order to install please use:
-          ${data.installs}
-          ### Licence
-          This project is licensed under:
-          ${liclink}
-          ### Contributing
-          ${data.contributing}
-          ### Testing
-          ${data.testing}
-          ### GIT Profile
-          This project can be found at : ${data/gitlink}
-          ### Questions
-          * If you have any questions about this application please email me at ${email}`;
+`# ${answers.title}
 
-module.exports = {readMe}  
+  ${renderLicenseBadge(answers.license)}
+
+
+---
+${answers.description}
+---
+## Table of Contents: 
+
+* [Installation](#Installation)
+* [Licence](#License)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Testing](#Testing)
+* [GIT Profile](# GIT Profile)
+* [Questions](#questions)
+---
+### Installation :exclamation:
+In order to install please use:
+ ${answers.installs}
+---
+### Contributing
+ ${answers.contributing}
+---
+### Licence
+This project is licensed under:
+ ${renderLicenseLink(answers.license)}
+---
+### Testing
+:${answers.tests}
+---
+### GIT Profile :link: 
+This project can be found at : ${answers.gitlink}
+---
+### Questions :question:
+* If you have any questions about this application please email me at ${answers.email}`;
+
+fs.writeFile(`./README.md`,readMe, error => error? console.log(error) : console.log(`File created`)) 
 };
 
+module.exports = {generateMarkdown}
 
